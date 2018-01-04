1e5d9fd86f768183fb836160fe40e4f683a71983
prokop7 <a.prokopev@innopolis.ru>
2018-01-04 20:42:19 +0300

    Refactored to AsyncHandler

---
+++ Kontur.ImageTransformer/AsyncHandler.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Kontur.ImageTransformer
+{
+    public class AsyncHandler
+    {
+        private static readonly IAlgorithm _threshold = new Threshold();
+        private static readonly IAlgorithm _greyscale = new Greyscale();
+        private static readonly IAlgorithm _sepia = new Sepia();
+
+        private enum MethodType
+        {
+            Grayscale,
+            Sepia,
+            Threshold,
+            None
+        }
+
+        public bool Handle(HttpListenerContext listenerContext)
+        {
+            var (method, x, y, width, height, threshold) = ParseUrl(listenerContext.Request.RawUrl);
+            if (method == MethodType.None)
+            {
+                listenerContext.Response.StatusCode = 400;
+                listenerContext.Response.Close();
+                return false;
+            }
+            var bitmap = GetBitmap(listenerContext, x, y, width, height);
+            if (bitmap == null) return false;
+
+            var(bitmapData, ptr, bytes, rgbValues) = GetData(bitmap);
+
+            Process(method, rgbValues, threshold);
+
+            StoreData(listenerContext, bitmap, bitmapData, ptr, bytes, rgbValues);
+            return true;
+        }
+
+        private static (MethodType, int, int, int, int, int) ParseUrl(string url)
+        {
+            var pattern =
+                new Regex("/process/(grayscale|threshold(\\(\\d+\\))|sepia)/(-?\\d+),(-?\\d+),(-?\\d+),(-?\\d+)");
+            try
+            {
+                var match = pattern.Match(url);
+                MethodType methodType;
+                var threshold = 0;
+                switch (match.Groups[1].Value)
+                {
+                    case "grayscale":
+                        methodType = MethodType.Grayscale;
+                        break;
+                    case "threshold":
+                        methodType = MethodType.Threshold;
+                        threshold = Convert.ToInt32(match.Groups[2].Value);
+                        if (threshold < 0 || threshold > 100)
+                            return (MethodType.None, 0, 0, 0, 0, 0);
+                        break;
+                    case "sepia":
+                        methodType = MethodType.Sepia;
+                        break;
+                    default:
+                        methodType = MethodType.None;
+                        break;
+                }
+                var x = Convert.ToInt32(match.Groups[3].Value);
+                var y = Convert.ToInt32(match.Groups[4].Value);
+                var width = Convert.ToInt32(match.Groups[5].Value);
+                var height = Convert.ToInt32(match.Groups[6].Value);
+                return (methodType, x, y, width, height, threshold);
+            }
+            catch (Exception e)
+            {
+                return (MethodType.None, 0, 0, 0, 0, 0);
+            }
+        }
+
+        private static Bitmap GetBitmap(HttpListenerContext listenerContext, int x, int y, int width, int height)
+        {
+            if (listenerContext.Request.ContentLength64 == 0)
+            {
+                listenerContext.Response.StatusCode = 400;
+                listenerContext.Response.Close();
+                return null;
+            }
+            var init = new Bitmap(listenerContext.Request.InputStream);
+            var x1 = Math.Min(x, x + width);
+            var x2 = Math.Max(x, x + width);
+            var y1 = Math.Min(y, y + height);
+            var y2 = Math.Max(y, y + height);
+            Bound(ref x1, maxBound: init.Width);
+            Bound(ref x2, maxBound: init.Width);
+            Bound(ref y1, maxBound: init.Height);
+            Bound(ref y2, maxBound: init.Height);
+
+            void Bound(ref int value, int minBound = 0, int maxBound = 0)
+            {
+                value = Math.Min(value, maxBound);
+                value = Math.Max(value, minBound);
+            }
+
+            width = x2 - x1;
+            height = y2 - y1;
+            if (width * height == 0)
+            {
+                listenerContext.Response.StatusCode = 204;
+                listenerContext.Response.Close();
+                return null;
+            }
+            return init.Clone(new Rectangle(x1, y1, width, height), init.PixelFormat);
+        }
+
+        private static (BitmapData, IntPtr, int, byte[]) GetData(Bitmap bitmap)
+        {
+            var bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite,
+                bitmap.PixelFormat, new BitmapData());
+            var ptr = bitmapData.Scan0;
+            var bytes = Math.Abs(bitmapData.Stride) * bitmap.Height;
+            var rgbValues = new byte[bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            return (bitmapData, ptr, bytes, rgbValues);
+        }
+
+        private static void Process(MethodType listenerContext, byte[] rgbValues, int threshold)
+        {
+            switch (listenerContext)
+            {
+                case MethodType.Grayscale:
+                    _greyscale.Process(rgbValues);
+                    break;
+                case MethodType.Sepia:
+                    _sepia.Process(rgbValues);
+                    break;
+                case MethodType.Threshold:
+                    _threshold.Process(rgbValues, threshold);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(listenerContext), listenerContext, null);
+            }
+        }
+        
+        private static void StoreData(HttpListenerContext listenerContext, Bitmap bitmap, BitmapData bitmapData, IntPtr ptr,
+            int bytes, byte[] rgbValues)
+        {
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            bitmap.UnlockBits(bitmapData);
+            bitmap.Save(listenerContext.Response.OutputStream, ImageFormat.Png);
+            listenerContext.Response.ContentType = "image/png";
+            listenerContext.Response.OutputStream.Close();
+            listenerContext.Response.StatusCode = (int) HttpStatusCode.OK;
+        }
+    }
+}
--- Kontur.ImageTransformer/AsyncHttpServer.cs
+++ Kontur.ImageTransformer/AsyncHttpServer.cs
@@ -91,173 +91,56 @@ namespace Kontur.ImageTransformer
             }
         }
 
+        private static int _numRequests = 0;
+        private static SpinLock _spinLock = new SpinLock(false);
+        private static bool _lock;
 
-        private readonly IAlgorithm _threshold = new Threshold();
-        private readonly IAlgorithm _greyscale = new Greyscale();
-        private readonly IAlgorithm _sepia = new Sepia();
 
-        private void Bound(ref int value, int minBound = 0, int maxBound = 0)
+        private static bool CheckContentLength(HttpListenerContext listenerContext)
         {
-            value = Math.Min(value, maxBound);
-            value = Math.Max(value, minBound);
+            if (listenerContext.Request.ContentLength64 / 1024 <= 200) return true;
+            listenerContext.Response.StatusCode = 400;
+            listenerContext.Response.Close();
+            return false;
         }
 
-        private Bitmap GetBitmap(HttpListenerContext listenerContext, int x, int y, int width, int height)
+        private static bool PreHandler(HttpListenerContext listenerContext)
         {
-            if (listenerContext.Request.ContentLength64 == 0)
+            _spinLock.Enter(ref _lock);
+            if (_numRequests >= 2)
             {
-                listenerContext.Response.StatusCode = 400;
+                _lock = false;
+                _spinLock.Exit();
+                listenerContext.Response.StatusCode = 429;
                 listenerContext.Response.Close();
-                return null;
+                return false;
             }
-            var init = new Bitmap(listenerContext.Request.InputStream);
-            var x1 = Math.Min(x, x + width);
-            var x2 = Math.Max(x, x + width);
-            var y1 = Math.Min(y, y + height);
-            var y2 = Math.Max(y, y + height);
-            Bound(ref x1, maxBound: init.Width);
-            Bound(ref x2, maxBound: init.Width);
-            Bound(ref y1, maxBound: init.Height);
-            Bound(ref y2, maxBound: init.Height);
-            width = x2 - x1;
-            height = y2 - y1;
-            if (width * height == 0)
-            {
-                listenerContext.Response.StatusCode = 204;
-                listenerContext.Response.Close();
-                return null;
-            }
-            return init.Clone(new Rectangle(x1, y1, width, height), init.PixelFormat);
+            _numRequests++;
+            _lock = false;
+            Console.Out.WriteLine(_numRequests);
+            _spinLock.Exit();
+            return CheckContentLength(listenerContext);
         }
 
-        private (BitmapData, IntPtr, int, byte[]) GetData(Bitmap bitmap)
+        private static void PostHandle(HttpListenerContext listenerContext)
         {
-            var bitmapData = bitmap.LockBits(
-                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                ImageLockMode.ReadWrite,
-                bitmap.PixelFormat, new BitmapData());
-            var ptr = bitmapData.Scan0;
-            var bytes = Math.Abs(bitmapData.Stride) * bitmap.Height;
-            var rgbValues = new byte[bytes];
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-            return (bitmapData, ptr, bytes, rgbValues);
+            _spinLock.Enter(ref _lock);
+            _numRequests--;
+            _lock = false;
+            _spinLock.Exit();
         }
 
-        private enum MethodType
+        private static async Task HandleContextAsync(HttpListenerContext listenerContext)
         {
-            Grayscale,
-            Sepia,
-            Threshold,
-            None
-        }
-
-        private async Task HandleContextAsync(HttpListenerContext listenerContext)
-        {
-            var stopwatch = Stopwatch.StartNew();
-            if (listenerContext.Request.ContentLength64 / 1024 > 200)
-            {
-                listenerContext.Response.StatusCode = 400;
-                listenerContext.Response.Close();
+            if (!PreHandler(listenerContext))
                 return;
-            }
-            var (method, x, y, width, height, threshold) = ParseUrl(listenerContext.Request.RawUrl);
-            if (method == MethodType.None)
-            {
-                listenerContext.Response.StatusCode = 400;
-                listenerContext.Response.Close();
+            var asyncHandler = new AsyncHandler();
+            await Task.Delay(10000);
+            if (!asyncHandler.Handle(listenerContext))
                 return;
-            }
-            var bitmap = GetBitmap(listenerContext, x, y, width, height);
-            if (bitmap == null) return;
-            Console.WriteLine(stopwatch.Elapsed);
-
-            var(bitmapData, ptr, bytes, rgbValues) = GetData(bitmap);
-            Console.WriteLine(stopwatch.Elapsed);
-
-            Process(method, rgbValues, threshold);
-            Console.WriteLine(stopwatch.Elapsed);
-
-            StoreData(listenerContext, bitmap, bitmapData, ptr, bytes, rgbValues);
-
-            Console.WriteLine(stopwatch.Elapsed);
-            stopwatch.Stop();
-            Console.Out.WriteLine();
+            PostHandle(listenerContext);
         }
 
-        
-        private int Year { get; set; }
-        
-        
-        private void IncYear() => Year++;
-
-        
-        private void StoreData(HttpListenerContext listenerContext, Bitmap bitmap, BitmapData bitmapData, IntPtr ptr,
-            int bytes, byte[] rgbValues)
-        {
-            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
-            bitmap.UnlockBits(bitmapData);
-            bitmap.Save(listenerContext.Response.OutputStream, ImageFormat.Png);
-            listenerContext.Response.ContentType = "image/png";
-            listenerContext.Response.OutputStream.Close();
-            listenerContext.Response.StatusCode = (int) HttpStatusCode.OK;
-        }
-
-        private void Process(MethodType listenerContext, byte[] rgbValues, int threshold)
-        {
-            switch (listenerContext)
-            {
-                case MethodType.Grayscale:
-                    _greyscale.Process(rgbValues);
-                    break;
-                case MethodType.Sepia:
-                    _sepia.Process(rgbValues);
-                    break;
-                case MethodType.Threshold:
-                    _threshold.Process(rgbValues, threshold);
-                    break;
-                case MethodType.None:
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(listenerContext), listenerContext, null);
-            }
-        }
-
-        private (MethodType, int, int, int, int, int) ParseUrl(string url)
-        {
-            var pattern = new Regex("/process/(grayscale|threshold(\\(\\d+\\))|sepia)/(-?\\d+),(-?\\d+),(-?\\d+),(-?\\d+)");
-            try
-            {
-                var match = pattern.Match(url);
-                var methodType = MethodType.None;
-                var threshold = 0;
-                switch (match.Groups[1].Value)
-                {
-                    case "grayscale":
-                        methodType = MethodType.Grayscale;
-                        break;
-                    case "threshold":
-                        methodType = MethodType.Threshold;
-                        threshold = Convert.ToInt32(match.Groups[2].Value);
-                        if (threshold < 0 || threshold > 100)
-                            return (MethodType.None, 0, 0, 0, 0, 0);
-                        break;
-                    case "sepia":
-                        methodType = MethodType.Sepia;
-                        break;
-                    default:
-                        methodType = MethodType.None;
-                        break;
-                }
-                var x = Convert.ToInt32(match.Groups[3].Value);
-                var y = Convert.ToInt32(match.Groups[4].Value);
-                var width = Convert.ToInt32(match.Groups[5].Value);
-                var height = Convert.ToInt32(match.Groups[6].Value);
-                return (methodType, x, y, width, height, threshold);
-            }
-            catch (Exception e)
-            {
-                return (MethodType.None, 0, 0, 0, 0, 0);
-            }
-        }
 
         private readonly HttpListener _listener;
 
